22a793f0c2a82c55f94c6490afa047b1397f707e
Pedro Henrique Gomes Lima <pedrohenriquegomeslima1@gmail.com>
2023-11-23 13:17:50 -0300

    animation header

--- Fanverso/javascript/header-animation-caregoria.js
+++ Fanverso/javascript/header-animation-caregoria.js
@@ -1,1 +1,8 @@
-let div_categorias = document.getElementById("")
+let div_categorias = document.querySelector("#categorias");
+let div_conteudo = document.querySelector("#menu .itens-menu");
+
+const add_active_to_contente = () => {
+    div_conteudo.classList.toggle("active");
+}
+
+div_categorias.addEventListener("click", add_active_to_contente);
